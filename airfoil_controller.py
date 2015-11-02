43d0473a00dfc0a60eb1ff4d9082701f39393bbb
saro6971 <sam.ronnlund@gmail.com>
2015-11-02 12:29:33 +0100

    simple database implementation

--- airfoil_controller.py
+++ airfoil_controller.py
@@ -1,10 +1,14 @@
 import time
 import os
+import pickledb
 from airfoil import airfoil
 from celery import group, subtask
 from flask import Flask, jsonify, request, redirect, render_template
 
 app = Flask(__name__)
+
+db = pickledb.load('Completed.db',False)
+
 
 def divide_input(start, stop, steps):
     diff = 0
@@ -32,7 +36,10 @@ def divide_input(start, stop, steps):
 
 def calc_airfoil(msh_input, airfoil_input):
 
+
+
     angles = divide_input(msh_input[0], msh_input[1], msh_input[2])
+    
     queue = [airfoil.s(x, msh_input[3], y, airfoil_input[0], airfoil_input[1],
              airfoil_input[2], airfoil_input[3]) for x in angles for y in range(0, msh_input[4]+1)]
     print queue
@@ -65,9 +72,13 @@ def forms():
 
     msh_input = [int(angle_min), int(angle_max), int(num_angles), int(nodes), int(refinement)]
     airfoil_input = [int(samples), float(viscosity), int(speed), int(time)]
-
-    res = calc_airfoil(msh_input, airfoil_input)
-
+    
+    if db.get(str(msh_input)+str(airfoil_input)) is None:
+        res = calc_airfoil(msh_input, airfoil_input)
+        db.set(str(msh_input)+str(airfoil_input),res)
+    else:
+        res = db.get(str(msh_input)+str(airfoil_input))
+    
     return render_template('results.html', data=res)
 
 if __name__ == '__main__':
